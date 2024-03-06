40f5fb99024d065857da14c9e88c3780ebc1f6d6
Kareena <kareenajain1299@gmail.com>
2024-03-06 20:04:18 +0530

    wip-doc_card

--- lib/module/home/widgets/doc_card.dart
+++ lib/module/home/widgets/doc_card.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class DocCard extends StatefulWidget {
   const DocCard({super.key});
@@ -10,15 +12,63 @@ class DocCard extends StatefulWidget {
 class _DocCardState extends State<DocCard> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        children: [
-          Image.network(
-            "https://pmmodiyojana.in/wp-content/uploads/2022/08/image-170.png",
-            height: 300,
-            width: 200,
-          )
-        ],
+    return Padding(
+      padding: const EdgeInsets.only(top: 10, bottom: 10),
+      child: Container(
+        child: Row(
+          children: [
+            Image.network(
+              "https://www.computerhope.com/jargon/d/doc.png",
+              height: 170,
+            ),
+            Container(
+              height: 170,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text("Document Name",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      )),
+                  const Text(
+                    "Size",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      OutlinedButton(
+                        onPressed: () {
+
+                        },
+                        child: Image.network(
+                            "https://cdn0.iconfinder.com/data/icons/multimedia-261/32/Send-512.png",
+                          height: 18,
+                          width: 18,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  OutlinedButton(
+                    onPressed: () {
+
+                    },
+                    child: const Icon(
+                      Icons.download,
+                      color: Colors.white,
+                      weight: 18,
+                      size: 20,
+                    )
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
